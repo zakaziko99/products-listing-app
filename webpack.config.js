845fec787e1236a8773849687f7bbc7019c11a95
Zakaria Amarifi <z.amarifi@dm73.net>
2018-12-15 11:43:39 +0100

    add sass loader and some css workarounds

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const path = require('path');
 const devMode = process.env.NODE_ENV !== 'production';
+const webpack = require('webpack');
+const autoprefixer = require('autoprefixer');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 let config = {
   entry : './src/index.js',
@@ -16,11 +19,41 @@ let config = {
         use: {
           loader: "babel-loader"
         }
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader"
+        ]
       }
     ]
   },
   devtool: devMode ? 'source-map' : false,
-  mode: devMode ? 'development' : 'production'
+  mode: devMode ? 'development' : 'production',
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+      path: path.resolve(__dirname, 'dist')
+    }),
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        postcss: [
+          autoprefixer({
+            browsers: [
+              '>1%',
+              'last 4 versions',
+              'not dead',
+              'not ie < 10'
+            ]
+          })
+        ]
+      }
+    })
+  ]
 };
 
 module.exports = config;
