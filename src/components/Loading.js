93b515e65e025de89616acb36fb559e1ded6cd02
Zakaria Amarifi <z.amarifi@dm73.net>
2018-12-15 13:12:41 +0100

    complete Loading component with styling

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -4,7 +4,11 @@ import './Loading.scss';
 class Loading extends Component {
   render() {
     return (
-      <div className="Loading">Loading</div>
+      <div className="Loading">
+        <div className="LoadingCercle">
+          <div className="LoadingSpinner"></div>
+        </div>
+      </div>
     );
   }
 }
